a098a3f77095d55687d8b70a1012f4170107f873
Faturar <faturardiansyah122@gmail.com>
2024-12-18 07:43:02 +0700

    adding contact page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
